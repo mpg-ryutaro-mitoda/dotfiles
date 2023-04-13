6bd7bb157d2887932c2c54cb73d3d71aac331638
mpg-ryutaro-mitoda <ryutaro.mitoda@medpeer.co.jp>
2023-04-13 13:45:48 +0900

    variables = "NONE"

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -26,6 +26,7 @@ require('github-theme').setup({
   comment_style	= 'NONE',
   function_style = 'NONE',
   keyword_style = 'NONE',
+  variables = "NONE"
 })
 vim.opt.termguicolors = true
 vim.cmd 'autocmd ColorScheme * highlight Normal ctermbg=none guibg=none'
