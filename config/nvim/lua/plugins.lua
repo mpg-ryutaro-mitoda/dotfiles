2c241b6564690755d8bcbd66b53747a4dcf54313
mpg-ryutaro-mitoda <ryutaro.mitoda@medpeer.co.jp>
2023-04-21 08:02:19 +0900

    init

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -31,11 +31,9 @@ require'packer'.startup(function()
   use 'norcalli/nvim-colorizer.lua'
   use 'lukas-reineke/indent-blankline.nvim'
   use 'nvim-treesitter/nvim-treesitter'
-  use 'cocopon/iceberg.vim'
   use { 'petertriho/nvim-scrollbar' }
   use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
   use { 'projekt0n/github-nvim-theme', tag = 'v0.0.7' }
-  use 'Hiroya-W/sequoia-moonlight.nvim'
   use { 'junegunn/fzf', run = ":call fzf#install()" }
   use { 'junegunn/fzf.vim' }
   use {'neoclide/coc.nvim', branch = 'release'}
